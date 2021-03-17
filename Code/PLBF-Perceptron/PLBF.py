9312c219d5ea092a336250f71f7246f6f91a2c78
Shizhe LIU <39071589+Josehokec@users.noreply.github.com>
2021-03-17 22:01:26 +0800

    try to use Perceptron Model
    however, its performance is not good

---
+++ Code/PLBF-Perceptron/PLBF.py
@@ -0,0 +1,82 @@
+import sys
+import pickle
+import math
+import random
+
+from BloomFilter import BestBloomFilter, BloomFilter
+from utils import *
+
+sys.path.append("../lib")
+
+"""
+add a initial filter
+"""
+
+
+class PLBF(object):
+    def __init__(self, model, data, using_Fpr=True, fp_rate=0.01, total_size=100000, model_size=int(70 * 1024 * 8),
+                 is_train=True):
+        self.model = model
+        self.threshold = 0.9
+        self.using_Fpr = using_Fpr
+        self.is_train = is_train
+
+        (s1, s2) = split_negatives(data, 0.7)
+        if self.is_train:
+            self.fit(data.positives, data.negatives)
+        else:
+            print("no model to load!!!")
+        if using_Fpr:
+            self.fp_rate = float(fp_rate)
+            self.create_best_bloom_filter(data, s2)
+        else:
+            self.m = total_size - model_size
+            self.create_bloom_filter(data, s2)
+
+    def check(self, item):
+        if self.model.predict(item) > self.threshold:
+            return True
+        return self.bloom_filter.check(item)
+
+    def create_best_bloom_filter(self, data, test_negatives):
+        print("Creating bloom filter")
+        self.get_threshold(test_negatives)
+        print("model threshold: %f" % self.threshold)
+
+        false_negatives = []
+        preds = self.model.predicts(data.positives)
+        for i in range(len(data.positives)):
+            if preds[i] <= self.threshold:
+                false_negatives.append(data.positives[i])
+        print("Number of false negatives at bloom time", len(false_negatives))
+        self.bloom_filter = BestBloomFilter(len(false_negatives), self.fp_rate / 2)
+        for fn in false_negatives:
+            self.bloom_filter.add(fn)
+        print("Created bloom filter")
+        print("hash function K: ", self.bloom_filter.hash_count)
+        print("bBF memory size: ", self.bloom_filter.size)
+
+    def fit(self, positives, negatives):
+        shuffled = shuffle_for_training(negatives, positives)
+        self.model.fit(shuffled[0], shuffled[1])
+        print("Done fitting")
+
+    # add data
+    def get_threshold(self, test_negatives):
+        fp_index = math.ceil((len(test_negatives) * (1 - self.fp_rate / 2)))
+        predictions = self.model.predicts(test_negatives)
+        predictions.sort()
+        """
+        predictions = self.model.predicts(test_negatives[0:10])
+        print(predictions)
+        result1 = list()
+        result2 = list()
+        for i in range(10):
+            result1.append(self.model.predict(test_negatives[i]))
+        for i in range(10):
+            result2.append(self.model.predict(data.positives[i]))
+        print('negative keys test: ', result1)
+        print('positive keys test: ', result2)
+        """
+
+        self.threshold = predictions[fp_index]
---
+++ Code/PLBF-Perceptron/run_PLBF.py
@@ -0,0 +1,40 @@
+import sys
+import os
+
+sys.path.append("../lib")
+
+from PerceptronModel import PerceptronModel, get_urldata_model
+from PLBF import PLBF
+from dataload import *
+from utils import *
+from time import *
+from tqdm import tqdm
+
+
+def test_urldata(total_size=100000, model_size=int(70 * 1024 * 8)):
+    positives, negatives = get_urldata()
+    begin_time = time()
+    data = Data(positives, negatives)
+    model = get_urldata_model()
+    plbf = PLBF(model, data, using_Fpr=True, total_size=total_size, model_size=model_size, is_train=True)
+    end_time = time()
+    run_time = end_time - begin_time
+    print('LBF buildtime：', run_time)
+
+    print("negatives fpr and query latency test...")
+    query_begin_time = time()
+    count = 0
+    # for i in tqdm(range(len(negatives)), ncols=50):
+    for i in range(len(negatives)):
+        if plbf.check(negatives[i]):
+            count += 1
+    query_end_time = time()
+    sum_query_time = query_end_time - query_begin_time
+    print("fpr:", 100 * count / (len(negatives)))
+    # query latency -> negative
+    print("query latency:", (10**6) * sum_query_time / (len(negatives)))
+
+
+if __name__ == '__main__':
+    # url test
+    test_urldata()
